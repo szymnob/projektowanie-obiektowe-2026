179e3c69d32de7962536156d00fe99de2b93d93c
szymnob <szymon6457@gmail.com>
2026-04-08 20:54:09 +0200

    lab3:  Initialize Spring Application

---
+++ lab3/src/main/kotlin/com/szymnob/lab3/Lab3Application.kt
@@ -0,0 +1,11 @@
+package com.szymnob.lab3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab3Application
+
+fun main(args: Array<String>) {
+	runApplication<Lab3Application>(*args)
+}
---
+++ lab3/src/test/kotlin/com/szymnob/lab3/Lab3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.szymnob.lab3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lab3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
