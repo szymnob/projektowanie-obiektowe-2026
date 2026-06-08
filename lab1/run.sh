b6212fd0ca22689d10b501ed8beadb657c87495e
szymnob <szymon6457@gmail.com>
2026-06-08 13:08:14 +0200

    lab1 readme

--- lab1/run.sh
+++ lab1/run.sh
@@ -6,5 +6,4 @@ echo "Budowanie obrazu Docker..."
 docker build -t $IMAGE_NAME .
 
 echo -e "\nUruchamianie aplikacji w kontenerze:\n"
-# --rm usuwa kontener automatycznie po zakończeniu działania
 docker run --rm $IMAGE_NAME
